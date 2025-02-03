e191f82bfc3557a9d2e253ecae4a1061371ff588
codeWmeku <mikko.dumaguin@lorma.edu>
2025-02-04 01:06:03 +0800

    Your meaningful message

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+function smoothScroll(target) {
+  const element = document.querySelector(target);
+  element.scrollIntoView({
+    behavior: 'smooth', 
+    block: 'start', 
+    inline: 'nearest' 
+  });
+}
+
+
+function toggleSidebar() {
+  document.getElementById("sidebar").classList.toggle("open");
+}
+
